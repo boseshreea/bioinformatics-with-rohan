b4d25de035c9b84546aac8a30defd69f54273eaf
RohanNathHERE <rohan.nath.here@gmail.com>
2022-10-11 21:12:02 +0530

    Changes

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*==========MENU SHOW Y HIDDEN==========*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*==========MENU SHOW==========*/
+/*Validate if constant exists*/
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+/*==========REMOVE MENU MOBILE==========*/
+
+/*==========ACCORDION SKILLS==========*/
+
+/*==========QUALIFICATION TABS==========*/
+
+/*==========SERVICES MODAL==========*/
